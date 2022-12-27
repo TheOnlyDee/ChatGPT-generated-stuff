673da23e4ecda22bee2b148cb20284e87bef5a34
TheOnlyDee <115394495+TheOnlyDee@users.noreply.github.com>
2022-12-27 17:19:11 +0100

    creato esempio.cxx

---
+++ math-functions/c++/esempio.cxx
@@ -0,0 +1,12 @@
+//Puoi utilizzare queste funzioni nei tuoi programmi inserendo il file con la libreria nella tua cartella di progetto e includendo l'intestazione nei tuoi file di codice sorgente:
+
+
+#include "math_functions.h"
+
+int main() {
+    double x = 2.0;
+    double y = 3.0;
+    double result = pow(x, y);
+    std::cout << result << std::endl;
+    return 0;
+}
