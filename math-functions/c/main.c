418c2f60f33f6eec3c2bde3801b1af659513bdde
TheOnlyDee <115394495+TheOnlyDee@users.noreply.github.com>
2022-12-27 17:11:27 +0100

    creata la cartella /c/ e main.c

---
+++ math-functions/c/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+// Funzione per il calcolo della radice quadrata di un numero
+double sqrt(double x) {
+  return sqrt(x);
+}
+
+// Funzione per il calcolo della potenza di un numero
+double pow(double x, double y) {
+  return pow(x, y);
+}
+
+// Funzione per il calcolo del logaritmo di un numero
+double log(double x) {
+  return log(x);
+}
