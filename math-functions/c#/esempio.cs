73c5e32ff7b858b5c0a032782e0a69995efa1da6
TheOnlyDee <115394495+TheOnlyDee@users.noreply.github.com>
2022-12-27 17:25:15 +0100

    create /c#/ and main.cs

---
+++ math-functions/c#/esempio.cs
@@ -0,0 +1,11 @@
+//Per utilizzare questa libreria, basta importare il namespace MathLibrary e chiamare le funzioni come se fossero membri della classe MathLibrary.
+//Ad esempio:      
+
+
+using MathLibrary;
+
+double root = MathLibrary.Sqrt(4);
+double power = MathLibrary.Power(2, 3);
+double log = MathLibrary.Log(10);
+double cube = MathLibrary.Cube(5);
+double area = MathLibrary.CircleArea(3);
