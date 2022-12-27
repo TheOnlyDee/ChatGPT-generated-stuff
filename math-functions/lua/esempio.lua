34ead5ff8c49e520daf7ea78c4f8493204ac55be
TheOnlyDee <115394495+TheOnlyDee@users.noreply.github.com>
2022-12-27 17:09:26 +0100

    Create esempio.lua

---
+++ math-functions/lua/esempio.lua
@@ -0,0 +1,22 @@
+--Potresti usare queste funzioni come segue:
+
+
+
+-- Calcola la radice quadrata di 4
+print(sqrt(4))  -- Output: 2.0
+
+-- Calcola 2^8
+print(pow(2, 8))  -- Output: 256
+
+-- Calcola la radice cubica di 8
+print(cbrt(8))  -- Output: 2.0
+
+-- Calcola il logaritmo in base 10 di 100
+print(log10(100))  -- Output: 2.0
+
+-- Calcola il logaritmo in base 2 di 16
+print(log2(16))  -- Output: 4.0
+
+-- Calcola il logaritmo naturale di 2
+print(ln(2))  -- Output: 0.693147180559945
+
