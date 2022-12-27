8e1caed8f3b34fefd1c165c3dd6fff7777c9bcfb
TheOnlyDee <115394495+TheOnlyDee@users.noreply.github.com>
2022-12-27 17:17:12 +0100

    created /c++/ and main.cxx

---
+++ math-functions/c++/main.cxx
@@ -0,0 +1,31 @@
+#include <cmath>
+
+// Calcola la radice quadrata di un numero
+double sqrt(double x) {
+    return std::sqrt(x);
+}
+
+// Calcola la potenza di un numero
+double pow(double base, double exponent) {
+    return std::pow(base, exponent);
+}
+
+// Calcola il valore assoluto di un numero
+double abs(double x) {
+    return std::abs(x);
+}
+
+// Calcola il seno di un angolo (in radianti)
+double sin(double x) {
+    return std::sin(x);
+}
+
+// Calcola il coseno di un angolo (in radianti)
+double cos(double x) {
+    return std::cos(x);
+}
+
+// Calcola il tangente di un angolo (in radianti)
+double tan(double x) {
+    return std::tan(x);
+}
