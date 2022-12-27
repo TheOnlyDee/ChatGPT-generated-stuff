110e97eca6e40ece310ac42ac4fc6cc286149cc6
TheOnlyDee <115394495+TheOnlyDee@users.noreply.github.com>
2022-12-27 17:08:04 +0100

    creata la cartella e main.lua

---
+++ math-functions/lua/main.lua
@@ -0,0 +1,29 @@
+-- Definisci la funzione per il calcolo della radice quadrata
+function sqrt(x)
+  return x^(0.5)
+end
+
+-- Definisci la funzione per il calcolo della potenza
+function pow(x, y)
+  return x^y
+end
+
+-- Definisci la funzione per il calcolo della radice cubica
+function cbrt(x)
+  return x^(1/3)
+end
+
+-- Definisci la funzione per il calcolo del logaritmo in base 10
+function log10(x)
+  return math.log(x, 10)
+end
+
+-- Definisci la funzione per il calcolo del logaritmo in base 2
+function log2(x)
+  return math.log(x, 2)
+end
+
+-- Definisci la funzione per il calcolo del logaritmo in base e (naturale)
+function ln(x)
+  return math.log(x)
+end
